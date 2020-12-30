01b5108295409ecad62ef1eee892b124533e07d0
Jodok Vieli <jvieli@student.ethz.ch>
2020-12-30 13:38:27 +0100

    draw the template board

---
+++ view.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gdamore/tcell"
+)
+
+// NewView returns a pointer to a new instance of View
+func NewView() *View {
+	view := new(View)
+	loader := NewLoader()
+	loader.load("boardAscii.txt")
+	view.boardTmpl = loader.lines
+
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatal("NewScreen error:", err)
+	}
+
+	view.screen = screen
+	view.screen.Clear()
+
+	return view
+}
+
+func (v *View) drawBoard() {
+	styleBoard := tcell.StyleDefault.Foreground(tcell.ColorLightGray).Background(tcell.ColorBlack)
+	for y, line := range v.boardTmpl {
+		for x, c := range line {
+			v.screen.SetContent(x, y, c, nil, styleBoard)
+		}
+	}
+}
+
+// RefreshScreen updates the view of the screen
+func (v *View) RefreshScreen() {
+	v.drawBoard()
+}
