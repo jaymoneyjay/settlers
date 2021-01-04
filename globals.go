395637700109c37360012996e19688c861df217a
Jodok Vieli <jvieli@student.ethz.ch>
2021-01-04 18:16:13 +0100

    model board and tiles

--- globals.go
+++ globals.go
@@ -10,6 +10,21 @@ const (
 	animationSpeed = 10 * time.Millisecond
 )
 
+// Resource is a resource players can yield
+// Example: wood, ore etc.
+type Resource int
+
+const (
+	desert Resource = iota - 1
+	water
+	wood
+	brick
+	lumber
+	wool
+	grain
+	ore
+)
+
 type (
 	// View is the display engine
 	View struct {
@@ -23,6 +38,29 @@ type (
 	// Engine is the game engine of the settlers of catan game
 	Engine struct {
 	}
+
+	// Tile is a single hexagon of the game board representing a resource
+	Tile struct {
+		resource Resource
+		number   int
+	}
+
+	// Vertex is a node in the middle of three tiles
+	// represents the location for buildings
+	Vertex struct {
+	}
+
+	// Edge is a link between two vertices
+	// represents the location to build streets
+	Edge struct {
+	}
+
+	// Board models the settlers of catan game boar
+	Board struct {
+		tiles    [][]Tile
+		edges    [][]Edge
+		vertices [][]Vertex
+	}
 )
 
 var (
@@ -30,4 +68,5 @@ var (
 	view   *View
 	loader *TxtLoader
 	screen tcell.Screen
+	board  *Board
 )
